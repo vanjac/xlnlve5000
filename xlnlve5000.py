c5561a18c0dd091676917c065797b1d8cb01135f
Jacob van't Hoog <vantjac@gmail.com>
2017-05-31 18:22:46 -0700

    Add property rates; change frame property to clipframe; preserve frame property when switching files

--- xlnlve5000.py
+++ xlnlve5000.py
@@ -7,10 +7,16 @@ sheet = None
 row = 1
 frame = -1
 channelProperties = {} # { channelNumber: { propertyName : value } }
+propertyRates = [ ] # [ (channelNumber, propertyName, rate) ]
 end = False
 
 capture = None
 fps = 30
+
+def initProperties():
+    channelProperties[0] = { }
+    channelProperties[0]["clipframe"] = 0
+    propertyRates.append((0, "clipframe", 1))
 
 def loadVideo(path):
     global row, sheet
@@ -18,9 +24,16 @@ def loadVideo(path):
     sheet = wb.get_sheet_by_name(wb.get_sheet_names()[0])
 
 def nextFrame():
-    global row, frame, end
+    global sheet, row, frame, channelProperties, propertyRates, end
 
     frame += 1
+
+    for r in propertyRates:
+        if r[0] in channelProperties:
+            channel = channelProperties[r[0]]
+            if r[1] in channel:
+                channel[r[1]] += r[2]
+                handlePropertyChange(r[0], r[1], channel[r[1]], rateChange=True)
 
     nextEventTime = int(sheet['A' + str(row + 1)].value)
     if nextEventTime == frame:
@@ -45,20 +58,23 @@ def nextFrame():
             channelProperties[channel][propertyName] = propertyValue
             handlePropertyChange(channel, propertyName, propertyValue)
 
-def handlePropertyChange(channel, name, value):
-    global capture, fps
+def handlePropertyChange(channel, name, value, rateChange=False):
+    global channelProperties, capture, fps
     if channel == 0 and name == "file":
         if capture is not None:
             capture.release()
         capture = cv2.VideoCapture(value)
         fps = float(capture.get(cv2.CAP_PROP_FPS))
         print("fps", fps)
-    elif channel == 0 and name == "frame":
+        f = channelProperties[0]["clipframe"]
+        capture.set(cv2.CAP_PROP_POS_MSEC, int(f) * (1000.0 / fps))
+    elif channel == 0 and name == "clipframe" and not rateChange:
         if capture is not None:
             capture.set(cv2.CAP_PROP_POS_MSEC, int(value) * (1000.0 / fps))
 
 
 if __name__ == "__main__":
+    initProperties()
     loadVideo(sys.argv[1])
     while not end:
         nextFrame()
